54eaec0bc71dcc14fffbd953771cfcd2bee95082
Oleksii Solomakha <alexssolomakha@gmail.com>
2019-10-01 02:24:55 +0300

    Create project logic and styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ $('input[type=text]').on('keypress', function(event) {
   if(event.which === 13) {
     let newItemText = $(this).val();
     $(this).val('');
-    $('ul').append(`<li>${newItemText} <span>X</span></li>`);
+    $('ul').append(`<li>${newItemText} <span><i class="fas fa-trash"></i></li>`);
   }
 });
 // Check of Todo
@@ -16,4 +16,8 @@ $('ul').on('click', 'span', function(event) {
   $(this).parent().fadeOut(500, function() {
     $(this).remove();
   });  
+});
+// Hide input
+$('.fa-pencil-alt').click(() => {
+  $('input[type=text]').fadeToggle();
 });
